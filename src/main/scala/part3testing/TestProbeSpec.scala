878f65491716b5478d598d03ae80c9f03c2c29aa
Brais Cidras <braisomary@gmail.com>
2023-10-06 20:32:04 +0200

    Test Probes

---
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -0,0 +1,107 @@
+package part3testing
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec")) with ImplicitSender with AnyWordSpecLike with BeforeAndAfterAll {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  import TestProbeSpec._
+  import Master._
+
+  "A master actor" should {
+    "register a slave" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+    }
+
+    "send the work to the slave actor" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workLoadString = "I love Akka"
+      master ! Work(workLoadString)
+
+      slave.expectMsg(SlaveWork(workLoadString, testActor)) // Remember testActor is the default injected actor who sends messages to Master Actor
+      slave.reply(WorkCompleted(3, testActor))
+
+      expectMsg(Report(3)) // TestActor receives Report(3)
+    }
+
+    "aggregate data correctly" in {
+      val master = system.actorOf(Props[Master])
+      val slave = TestProbe("slave")
+
+      master ! Register(slave.ref)
+      expectMsg(RegistrationAck)
+
+      val workLoadString = "I love Akka"
+      master ! Work(workLoadString)
+      master ! Work(workLoadString)
+
+      // In the meantime I don't have a slave actor
+      slave.receiveWhile() {
+        // `` guaranties it receives exactly the concept between ``
+        case SlaveWork(`workLoadString`, `testActor`) => slave.reply(WorkCompleted(3, testActor))
+      }
+      expectMsg(Report(3)) // TestActor receives Report(3)
+      expectMsg(Report(6))
+    }
+  }
+}
+
+object TestProbeSpec {
+  // Scenario
+  /*
+    Word counting actor hierarchy master-slave
+
+    send some work to the master
+      - master sends the slave the piece of work
+      - slave processes the work and replies to master
+      - master aggregates the result
+    master sends the total count to original requester
+   */
+
+  object Master {
+    case class Work(text: String)
+    case class SlaveWork(text: String, originalRequester: ActorRef)
+    case class WorkCompleted(count: Int, originalRequester: ActorRef)
+    case class Register(slaveRef: ActorRef)
+    case object RegistrationAck
+    case class Report(totalCount: Int)
+  }
+
+  class Master extends Actor {
+    import Master._
+
+    override def receive: Receive = {
+      case Register(slaveRef) =>
+        sender() ! RegistrationAck
+        context.become(online(slaveRef, 0))
+      case _ => // Ignore
+    }
+
+    def online(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequester) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequester ! Report(newTotalWordCount)
+        context.become(online(slaveRef, newTotalWordCount))
+    }
+  }
+
+  // class Slave extends Actor ...
+}
+
+
